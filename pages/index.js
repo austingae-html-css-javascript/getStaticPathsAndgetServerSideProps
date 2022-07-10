57bf2fba553b5c069c734ac283f8afe1be2b8c05
Austin Gae <austingae@gmail.com>
2022-07-09 17:55:36 -0700

    build again to show new data

--- pages/index.js
+++ pages/index.js
@@ -43,10 +43,11 @@ export async function getStaticProps() {
 getStaticProps will get the data at only build time. 
 Therefore, if I add new data, then the website will not show the new data.
 To show the new data, I will need to do build again.
+Correct!!!
+
 
 getServerSideProps will get the data before the website page loads. 
 Therefore, if I add new data, and I reload the page, then the website page will
 show the new data. 
 
-I am correct on both!
 */
